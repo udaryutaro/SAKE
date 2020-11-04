ee9291c26b627288b42732c1fd86070edbdf3d36
vagrant <vagrant@localhost.localdomain>
2020-11-04 07:37:50 +0000

    (add)favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,19 @@
 class FavoritesController < ApplicationController
+
+	def create
+		otumami = Otumami.find(params[:otumami_id])
+		favorite = Favorite.new(params[:id])
+		favorite.otumami_id = otumami.id
+		favorite.user_id = current_user.id
+		favorite.save
+		redirect_back(fallback_location:otumamis_path)
+	end
+
+	def destroy
+		otumami = Otumami.find(params[:otumami_id])
+		favorite = current_user.favorites.find_by(otumami_id: otumami.id)
+    	favorite.destroy
+    	 redirect_back(fallback_location:otumamis_path)
+	end
+
 end
--- app/models/otumami.rb
+++ app/models/otumami.rb
@@ -4,6 +4,11 @@ class Otumami < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
 
+	#いいね機能
+	def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+    end
+
 	belongs_to :user
 	belongs_to :otumami_genre
 
