bf31b718d050135e12c499d446625c1aa59a1e11
vagrant <vagrant@localhost.localdomain>
2020-12-15 07:10:15 +0000

    fix_otumamigenre_new_page

--- app/controllers/otumami_genres_controller.rb
+++ app/controllers/otumami_genres_controller.rb
@@ -6,8 +6,22 @@ class OtumamiGenresController < ApplicationController
 
 	def create
 		otumami_genre = OtumamiGenre.new(otumami_genre_params)
-		otumami_genre.save
-		redirect_to otumamis_top_path
+
+		#otumami_genre.save
+		#redirect_to otumamis_top_path
+
+		#####
+		otumami_genre_check = OtumamiGenre.find_by(name: otumami_genre.name)
+		if otumami_genre_check == nil
+			# 名前が存在しない
+			otumami_genre.save
+			redirect_to otumamis_top_path
+		else
+			# 名前がすでに存在する
+			# なんかえらーの処理
+			flash[:error] = "すでに存在するジャンルです。"
+			redirect_to new_otumami_genre_path
+		end
 	end
 
 	private
